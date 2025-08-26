1a69e24d9cef8e5272fd989c797cba112d623eac
sonseongmin <sungmin2959@gmail.com>
2025-08-26 22:13:57 +0900

    db

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # SQLite 경로 (파일형 DB)
-SQLALCHEMY_DATABASE_URL = "sqlite:///./dev.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./data/dev.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
