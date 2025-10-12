927e0b84d317ea72af67ccdc2db78a9bfd933259
sonseongmin <sungmin2959@gmail.com>
2025-10-13 05:43:33 +0900

    cors

--- app/main.py
+++ app/main.py
@@ -60,7 +60,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 또는 ["http://localhost:3000", "http://127.0.0.1:8000"]
+    allow_origins=["http://13.125.219.3"],  # 또는 ["http://localhost:3000", "http://127.0.0.1:8000"]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
